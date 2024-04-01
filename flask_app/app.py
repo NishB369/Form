17b5453705c37a11ac46c8c7814557a52f7f663d
NishB369 <nishbcodes@gmail.com>
2024-04-01 14:28:39 +0530

    first commit

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,42 +1,31 @@
 from flask import Flask, render_template, request
 import mysql.connector
 
-app = Flask(__name__)
+app=Flask(__name__)
 
-# MySQL Configuration
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="3690",
-    database="form"
-)
-
-mycursor = mydb.cursor()
+Db=mysql.connector.connect(host="localhost", user="root", password="3690", database="form")
+Cursor=Db.cursor()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        stud_name = request.form['studentName']
-        father_name = request.form['fatherName']
-        mother_name = request.form['motherName']
-        phone_number = request.form['phoneNumber']
-        email = request.form['email']
-        dob = request.form['dob']
-        address = request.form['address']
-        b_grp = request.form['bloodGroup']
-        dept = request.form['department']
-        course = request.form['course']
-        passwd = request.form['password']
-
-        # Insert into MySQL
-        sql = "INSERT INTO form_input (stud_name, father_name, mother_name, phone_number, email, dob, address, b_grp, dept, course, passwd) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        val = (stud_name, father_name, mother_name, phone_number, email, dob, address, b_grp, dept, course, passwd)
-        mycursor.execute(sql, val)
-        mydb.commit()
-
-        return "Registration successful!"
-
+    if request.method=='POST':
+        stud_name=request.form['studentName']
+        father_name=request.form['fatherName']
+        mother_name=request.form['motherName']
+        phone_number=request.form['phoneNumber']
+        email=request.form['email']
+        dob=request.form['dob']
+        address=request.form['address']
+        b_grp=request.form['bloodGroup']
+        dept=request.form['department']
+        course=request.form['course']
+        passwd=request.form['password']
+        sql="insert into form_input (stud_name, father_name, mother_name, phone_number, email, dob, address, b_grp, dept, course, passwd) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        val=(stud_name, father_name, mother_name, phone_number, email, dob, address, b_grp, dept, course, passwd)
+        Cursor.execute(sql, val)
+        Db.commit()
+        return "Registration Completed! Thank You."
     return render_template('index.html')
 
-if __name__ == '__main__':
+if __name__=='__main__':
     app.run(debug=True)
